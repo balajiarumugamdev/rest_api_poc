3f3811910b859da0bb52eee49590ac10a0e0c08c
balaji.a@emphaticsense.com <balaji_arumugam@outlook.com>
2025-05-28 23:26:27 +0530

    feat: delete and update user

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,4 +1,4 @@
-import { getUsers } from "../db/users";
+import { deleteUserById, getUserById, getUsers } from "../db/users";
 import express from "express";
 
 export const getAllUsers = async (
@@ -8,6 +8,45 @@ export const getAllUsers = async (
   try {
     const users = await getUsers();
     return res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);
+  }
+};
+
+export const deleteUser = async (
+  req: express.Request,
+  res: express.Response
+): Promise<any> => {
+  try {
+    const { id } = req.params;
+
+    const deletedUser = await deleteUserById(id);
+
+    return res.json(deletedUser);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);
+  }
+};
+
+export const updateUser = async (
+  req: express.Request,
+  res: express.Response
+): Promise<any> => {
+  try {
+    const { id } = req.params;
+    const { username } = req.body;
+
+    if (!username) {
+      return res.sendStatus(400);
+    }
+
+    const user = await getUserById(id);
+    user.username = username;
+    await user.save();
+
+    return res.status(200).json(user);
   } catch (error) {
     console.log(error);
     return res.sendStatus(500);
--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,25 +1,51 @@
-import { getUserBySessionToken } from 'db/users';
-import express from 'express';
-import {get, merge } from 'lodash'
+import { getUserBySessionToken } from "../db/users";
+import express from "express";
+import { get, merge } from "lodash";
 
-export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction): Promise<any> => {
-    try {
-        const sessionToken = req.cookies['BALAJI_AUTH'];
+export const isAuthenticated = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+): Promise<any> => {
+  try {
+    const sessionToken = req.cookies["BALAJI_AUTH"];
 
-        if (!sessionToken) {
-            return res.sendStatus(401);
-        }
-        const existingUser = await getUserBySessionToken(sessionToken);
-
-        if (!existingUser) {
-            return res.sendStatus(401);
-        }
-
-        merge(req, { identity: existingUser });
-
-        return next();
-    } catch (error) {
-        console.log(error);
-        return res.sendStatus(500);
+    if (!sessionToken) {
+      return res.sendStatus(401);
     }
-}
+    const existingUser = await getUserBySessionToken(sessionToken);
+
+    if (!existingUser) {
+      return res.sendStatus(401);
+    }
+
+    merge(req, { identity: existingUser });
+
+    return next();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);
+  }
+};
+
+export const isOwner = (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+): any => {
+  try {
+    const { id } = req.params;
+    const currentUserId = get(req, "identity_id") as string;
+
+    if (!currentUserId) {
+      return res.sendStatus(401);
+    }
+    if (currentUserId.toString() !== id) {
+      return res.sendStatus(401);
+    }
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);
+  }
+};
